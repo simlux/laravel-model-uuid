859fe19292dcbb9dc2f183adcda340aa5cb8d067
Simon Kleeschulte <simlux78@gmail.com>
2017-08-26 09:46:54 +0200

    added uuid class and model trait

---
+++ src/Uuid.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Simlux\LaravelModelUuid;
+
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+/**
+ * Class Uuid
+ *
+ * @package InspectYourWeb\Uuid
+ */
+class Uuid
+{
+    /**
+     * @return string
+     */
+    public static function generate(): string
+    {
+        return RamseyUuid::uuid4()->toString();
+    }
+
+    /**
+     * @param string $uuid
+     *
+     * @return bool
+     */
+    public static function checkSyntax(string $uuid): bool
+    {
+        return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) === 1;
+    }
+}
---
+++ src/UuidModelTrait.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace Simlux\LaravelModelUuid;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+/**
+ * Class UuidModelTrait
+ *
+ * @package InspectYourWeb\Uuid
+ */
+trait UuidModelTrait
+{
+
+    /**
+     * The "booting" method of the trait.
+     *
+     * @return void
+     */
+    protected static function bootUuidModelTrait()
+    {
+        static::creating(function (Model $model) {
+            $model->uuid = Uuid::generate();
+        });
+
+        static::saving(function (Model $model) {
+            $originalUuid = $model->getOriginal('uuid');
+            if ($model->uuid !== $originalUuid) {
+                $model->uuid = $originalUuid;
+            }
+        });
+    }
+
+    /**
+     * Scope a query to only include models matching the supplied UUID.
+     * Returns the model by default, or supply a second flag `false` to get the Query Builder instance.
+     *
+     * @param  Builder $query The Query Builder instance.
+     * @param  string  $uuid  The UUID of the model.
+     * @param  bool    $first Returns the model by default, or set to `false` to chain for query builder.
+     *
+     * @throws ModelNotFoundException
+     *
+     * @return Model|Builder
+     */
+    public function scopeUuid(Builder $query, string $uuid, bool $first = true)
+    {
+        if (!is_string($uuid) || !Uuid::checkSyntax($uuid)) {
+            throw (new ModelNotFoundException())->setModel(get_class($this));
+        }
+
+        $search = $query->where('uuid', $uuid);
+
+        return $first ? $search->firstOrFail() : $search;
+    }
+
+    /**
+     * Scope a query to only include models matching the supplied ID or UUID.
+     * Returns the model by default, or supply a second flag `false` to get the Query Builder instance.
+     *
+     * @param  Builder $query    The Query Builder instance.
+     * @param  string  $idOrUuid The UUID of the model.
+     * @param  bool    $first    Returns the model by default, or set to `false` to chain for query builder.
+     *
+     * @throws ModelNotFoundException
+     *
+     * @return Model|Builder
+     */
+    public function scopeIdOrUuId(Builder $query, $idOrUuid, bool $first = true)
+    {
+        if (!is_string($idOrUuid) && !is_numeric($idOrUuid)) {
+            throw (new ModelNotFoundException)->setModel(get_class($this));
+        }
+
+        if (!Uuid::checkSyntax($idOrUuid) && preg_match('/^[0-9]+$/', $idOrUuid) !== 1) {
+            throw (new ModelNotFoundException)->setModel(get_class($this));
+        }
+
+        $search = $query->where(function ($query) use ($idOrUuid) {
+            $query->where('id', $idOrUuid)
+                ->orWhere('uuid', $idOrUuid);
+        });
+
+        return $first ? $search->firstOrFail() : $search;
+    }
+}
